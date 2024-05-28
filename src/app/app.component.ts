c5d68214693abb8fad7a45b98da736595dfc2f0f
Terry Tan <fuling19.me@gmail.com>
2024-05-28 22:47:55 +0800

    add husky and lint-staged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title:  any = 'frontend-formatter-demo';
+  title = 'frontend-formatter-demo';
 }
